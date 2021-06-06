ad52a43892eb86cdbd0c6e0bc1d37ddec50b2057
karan yadav <47856276+karan68@users.noreply.github.com>
2021-06-06 19:16:14 +0530

    Add files via upload

---
+++ BulkyBook.Models/ApplicationUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BulkyBook.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+
+        public int? CompanyId { get; set; }//we added ? as if the user is not in a company then it's company will be 
+                                           //null else he will be assigned a company id
+
+        [ForeignKey("CompanyId")]
+        public Company Company { get; set; }
+
+        [NotMapped]
+        public string Role { get; set; } // we are not adding this to our database
+    }
+}
---
+++ BulkyBook.Models/ShoppingCart.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BulkyBook.Models
+{
+
+    public class ShoppingCart
+    {
+
+        public ShoppingCart()
+        {
+            Count = 1; // to keep the value to 1
+        }
+        [Key]
+        public int Id { get; set; }
+        public string ApplicationUserId { get; set; } //who is buying
+        [ForeignKey("ApplicationId")]
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public int ProductId { get; set; } //what  is buying
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+
+        [Range(1, 1000, ErrorMessage = "Please enter a value between 1 to 1000")]
+        public int Count { get; set; } //how many is buying
+
+        [NotMapped]
+        public double Price { get; set; }
+
+    }
+}
