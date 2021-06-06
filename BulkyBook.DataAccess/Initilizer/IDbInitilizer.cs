e9367d8e788495c43226b08158ed1184274f0778
karan yadav <47856276+karan68@users.noreply.github.com>
2021-06-06 19:13:35 +0530

    Add files via upload

---
+++ BulkyBook.DataAccess/Initilizer/IDbInitilizer.cs
@@ -0,0 +1,14 @@
+﻿using BulkyBook.DataAccess.Data;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Initilizer
+{
+  public interface IDbInitilizer
+    {
+
+        void Initialize();
+    }
+}
